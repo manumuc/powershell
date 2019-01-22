ee4549abdd7f7e53cccd7309cd59e863869483f0
manumuc <33813467+manumuc@users.noreply.github.com>
2019-01-22 20:45:48 +0100

    Create fl-dl.bat

---
+++ fl-dl.bat
@@ -0,0 +1,33 @@
+!/bin/bash
+#Flashplayer
+logfile=download_log
+
+#
+#
+https://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.101/install_flash_player_ax.exe
+https://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.101/install_flash_player.exe
+https://fpdownload.macromedia.com/get/flashplayer/pdc/32.0.0.101/install_flash_player_ppapi.exe
+ 
+
+ActiveX https://fpdownload.adobe.com/get/flashplayer/distyfp/current/win/
+NPAPI https://fpdownload.adobe.com/get/flashplayer/distyfp/current/win/install_flash_player_27_plugin.msi 
+PPAPI https://fpdownload.adobe.com/get/flashplayer/distyfp/current/win/install_flash_player_27_ppapi.msi 
+
+fplayer_mainlink='https://fpdownload.macromedia.com/pub/flashplayer/get/'
+fplayer_version=`curl --silent 'http://get.adobe.com/flashplayer/about/' | grep -o '[0-9]\{2\}\.[0-9]\.[0-9]\.[0-9]\{3\}' | sort -u -V | tail -1`
+fplayer_main=`echo $fplayer_version | cut -c1-2`
+f_activex='install_flash_player_'$fplayer_main'_active_x.msi'
+f_plugin='install_flash_player_'$fplayer_main'_plugin.msi'
+f_ppapi='install_flash_player_'$fplayer_main'_ppapi.msi'
+
+echo Download Flashplayer ActiveX $fplayer_version >>$logfile
+echo --------------------- >>$logfile
+wget -nv -a $logfile -nc -P flashplayer_$fplayer_version $fplayer_mainlink$fplayer_version/$f_activex
+
+echo Download Flashplayer Plugin $fplayer_version >>$logfile
+echo --------------------- >>$logfile
+wget -nv -a $logfile -nc -P flashplayer_$fplayer_version $fplayer_mainlink$fplayer_version/$f_plugin
+
+echo Download Flashplayer NPAPI $fplayer_version >>$logfile
+echo --------------------- >>$logfile
+wget -nv -a $logfile -nc -P flashplayer_$fplayer_version $fplayer_mainlink$fplayer_version/$f_ppapi
